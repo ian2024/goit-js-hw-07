638f0cbe07b9697a5d54beb14def09628e9fff67
ian2024 <ostapenkoyan@yahoo.com>
2022-09-28 15:47:41 +0300

    better version

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,19 +6,12 @@ const galleryContainer = document.querySelector('.gallery');
 function createImageGallery(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `
-    <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img
-            class="gallery__image"
-            src="${preview}"
-            alt="${description}"
-            />
-        </a>
-    </div>
-    `;
-    })
-    .join(``);
+      return `<li class="gallery__item">
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+  </li>`
+    }).join(``);
 }
 
 galleryContainer.insertAdjacentHTML('beforeend', createImageGallery(galleryItems));
